bed10753f3f4a93ec6a8ee4cdc85ab0bfebf86df
Nethmika-LK <nethumd65@gmail.com>
2025-07-04 22:20:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -128,7 +128,7 @@ const port = process.env.PORT || 9090;
 │    https://github.com/Jester36/SENU-MD  
 ╰─🚀 *POWERED BY JESTER-OFC*`;
 
-    conn.sendMessage(conn.user.id,{ image: { url: `https://files.catbox.moe/ke8nul.jpg` }, caption: up })
+    conn.sendMessage(conn.user.id,{ image: { url: `https://i.ibb.co/RGLKzshm/SulaMd.jpg` }, caption: up })
   }
   })
   conn.ev.on('creds.update', saveCreds)
